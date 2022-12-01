e0d5d571e502778521e05657c536fc798f94bec3
Deepto Debnath <dd1929@protonmail.com>
2022-12-02 01:45:11 +0530

    Add files via upload

---
+++ ios/src/org/dd1929/pink_elephant/IOSLauncher.java
@@ -0,0 +1,22 @@
+package org.dd1929.pink_elephant;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import org.dd1929.pink_elephant.PinkElephant;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new PinkElephant(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}

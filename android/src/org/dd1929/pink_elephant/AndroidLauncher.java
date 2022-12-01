b72455697ac1b1bb2f0e124708f69d9b2e062d81
Deepto Debnath <dd1929@protonmail.com>
2022-12-02 01:51:46 +0530

    Add files via upload

---
+++ android/src/org/dd1929/pink_elephant/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package org.dd1929.pink_elephant;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import org.dd1929.pink_elephant.PinkElephant;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PinkElephant(), config);
+	}
+}
